f5555d4d581b11057884ae0fec4bcd5049ca6f6e
Lin Dong <ldong@lins-mbp-3.corp.zscaler.com>
2016-02-14 16:51:33 -0800

    Update script.js
    1. Add eslinter
    2. Add multiple camera selection

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,56 @@
-(function(){
+(function() { 
   var isAudio = false;
   var isVideo = true;
   var localVideo = document.getElementById('localVideo');
+  var videoSource = document.getElementById('camera');
 
-  var onSuccess = function(stream){
+  function getCameras(sourceInfos) {
+    for (var i=0; i< sourceInfos.length; ++i) {
+      var sourceInfo = sourceInfos[i];
+      var option = document.createElement('option');
+      option.value = sourceInfo.id;
+      if (sourceInfo.kind === 'video') {
+        option.text = sourceInfo.label || 'camera ' + (videoSource.length + 1);
+        videoSource.appendChild(option);
+      }
+    }
+  };
+
+  var startStream = function() {
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    var constraints = {
+      audio: isAudio,
+      video: {
+        mandatory: {
+          maxWidth: 640,
+          maxHeight: 640,
+          minHeight: 360,
+          maxHeight: 480
+        },
+        optiona: [{
+            sourceId: videoSource
+          }
+        ]
+      }
+    };
+
+    navigator.getUserMedia(constraints, onSuccess, onError);
+  }
+
+  var onSuccess = function(stream) {
     console.log('Success');
     localVideo.src = window.URL.createObjectURL(stream);
     localVideo.className = 'grayscale_filter';
     localVideo.play();
   };
 
-  var onError = function(error){
+  var onError = function(error) {
     console.log('Error', error);
 
   };
 
-  navigator.getUserMedia = navigator.getUserMedia
-    || navigator.webkitGetUserMedia
-    || navigator.mozGetUserMedia;
-
-  var constraints = {
-    audio: isAudio,
-    video: {
-      mandatory: {
-        maxWidth: 640,
-        maxHeight: 640,
-        minHeight: 360,
-        maxHeight: 480
-      }
-    }
-  };
-
-  navigator.getUserMedia(constraints, onSuccess, onError);
+  // MediaStreamTrack.getSources is deprecated
+  MediaStreamTrack.getSources(getCameras);
+  startStream();
 }());
